73c97b6cd188cd9d4ed5c970d785e7f460014041
Chrisbchicken <chrsbnjmn@gmail.com>
2023-12-06 00:49:00 -0600

    temporarily commenting out a component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,19 @@
-import { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
+// import { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
 
-type ButtonProps = {
-  small?: boolean;
-  gray?: boolean;
-  className?: string;
-} & DetailedHTMLProps<
-  ButtonHTMLAttributes<HTMLButtonElement>,
-  HTMLButtonElement
->;
+// type ButtonProps = {
+//   small?: boolean;
+//   gray?: boolean;
+//   className?: string;
+// } & DetailedHTMLProps<
+//   ButtonHTMLAttributes<HTMLButtonElement>,
+//   HTMLButtonElement
+// >;
 
-export default function ({
-  small = false,
-  gray = false,
-  className = "",
-  ...props
-}: ButtonProps) {
-  return <button>{/*...props */}</button>;
-}
+// export default function ({
+//   small = false,
+//   gray = false,
+//   className = "",
+//   ...props
+// }: ButtonProps) {
+//   return <button>{/*...props */}</button>;
+// }
